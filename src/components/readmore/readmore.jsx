c1733df737d35d3b83d3d0bf0109ba250cd5d1f8
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-08-11 02:07:26 +0500

    set text align left

--- src/components/readmore/readmore.jsx
+++ src/components/readmore/readmore.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
 
 const ReadMore = ({ children }) => {
-    const text = children;
-    const [isReadMore, setIsReadMore] = useState(true);
-    const toggleReadMore = () => {
-      setIsReadMore(!isReadMore);
-    };
-    return (
-      <p className="">
-        {isReadMore ? text.slice(0, 415) : text}.
-        <button onClick={toggleReadMore} className="read-or-hide text-red-600 pl-2">
-          {isReadMore ? "Read More" : "Show Less"}
-        </button>
-      </p>
-    );
+  const text = children;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
   };
+  return (
+    <p>
+      {isReadMore ? text.slice(0, 415) : text}.
+      <button onClick={toggleReadMore} className="read-or-hide text-red-600 pl-2">
+        {isReadMore ? "Read More" : "Show Less"}
+      </button>
+    </p>
+  );
+};
 
 export default ReadMore
