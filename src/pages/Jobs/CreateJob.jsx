e5b0a022621ad15577222e5aa0e4526f31cdd941
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-08-20 01:45:58 +0500

    create form

--- src/pages/Jobs/CreateJob.jsx
+++ src/pages/Jobs/CreateJob.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Flatpickr from 'react-flatpickr';
 const CreateJob = () => {
+    const [jobModel, setjobModel] = useState({
+        expiryDate: new Date()
+    })
+    const handleExpiryDateChange = (date) => {
+        setjobModel((prevmodel) => ({
+            ...prevmodel,
+            expiryDate: date
+        }))
+    }
+    // ****************** Flatpicker Content ***********
+    const options = {
+
+        static: true,
+        monthSelectorType: 'static',
+        enableTime: false,
+        dateFormat: 'M j, Y',
+        defaultDate: [new Date().setDate(new Date().getDate() - 6), new Date()],
+        prevArrow: '<svg class="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M5.4 10.8l1.4-1.4-4-4 4-4L5.4 0 0 5.4z" /></svg>',
+        nextArrow: '<svg class="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M1.4 10.8L0 9.4l4-4-4-4L1.4 0l5.4 5.4z" /></svg>',
+        // onReady: (selectedDates, dateStr, instance) => {
+        //     instance.element.value = dateStr.replace('to', '-');
+        //     const customClass = (align) ? align : '';
+        //     instance.calendarContainer.classList.add(`flatpickr-${customClass}`);
+        // },
+        // onChange: (selectedDates, dateStr, instance) => {
+        //     instance.element.value = dateStr.replace('to', '-');
+        // },
+    }
     return (
-        <div className="px-4 sm:px-6 lg:px-8 py-8 w-full  mx-auto">
-            <h1>CREATE JOB</h1>
+        <div className='bscontainer-fluid'>
+            <div className='row'>
+                <div className='col-lg-4'>
+                    <div>
+                        <label className="block text-sm font-medium mb-1" htmlFor="job_title">Job Title</label>
+                        <input name='job_title' id="job_title" className="form-input w-full" type="text" />
+                    </div>
+                </div>
+                <div className='col-lg-4'>
+                    <div>
+                        <label className="block text-sm font-medium mb-1" htmlFor="salary">Salary</label>
+                        <input name='salary' id="salary" className="form-input w-full" type="text" />
+                    </div>
+                </div>
+                <div className='col-lg-4'>
+                    <div>
+                        <label className="block text-sm font-medium mb-1" htmlFor="description">Description</label>
+                        <input name='description' id="description" className="form-input w-full" type="text" />
+                    </div>
+                </div>
+                <div className='col-lg-4'>
+                    <div>
+                        <label className="block text-sm font-medium mb-1" htmlFor="employer">Employer</label>
+                        <input name='employer' id="employer" className="form-input w-full" type="text" />
+                    </div>
+                </div>
+                <div className='col-lg-4'>
+                    <label className="block text-sm font-medium mb-1" htmlFor="country">jobtype</label>
+                    <select id="country" className="form-select w-full">
+                        <option>Full Time</option>
+                        <option>Part Time</option>
+                        <option>Hybrid</option>
+                    </select>
+                </div>
+                <div className='col-lg-4'>
+                    <label className="block text-sm font-medium mb-1" htmlFor="country">Job Status</label>
+                    <select id="country" className="form-select w-full">
+                        <option>Active</option>
+                        <option>Deactive</option>
+                    </select>
+                </div>
+                <div className='col-lg-4'>
+                    <label className="block text-sm font-medium mb-1" htmlFor="country">Job Class</label>
+                    <select id="country" className="form-select w-full">
+                        <option>Physical</option>
+                        <option>Rermote</option>
+                    </select>
+                </div>
+                <div className='col-lg-4'>
+                    <label className="block text-sm font-medium mb-1"  >Expiry Date</label>
+                    <div className="relative">
+                        <Flatpickr data-enable-time
+                            value={jobModel.expiryDate}
+                            onChange={handleExpiryDateChange} className="form-input w-full pl-9 text-slate-500 hover:text-slate-600 font-medium focus:border-slate-300  " options={options} />
+                        <div className="absolute inset-0 right-auto flex items-center pointer-events-none">
+                            <svg className="w-4 h-4 fill-current text-slate-500 ml-3" viewBox="0 0 16 16">
+                                <path d="M15 2h-2V0h-2v2H9V0H7v2H5V0H3v2H1a1 1 0 00-1 1v12a1 1 0 001 1h14a1 1 0 001-1V3a1 1 0 00-1-1zm-1 12H2V6h12v8z" />
+                            </svg>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
         </div>
     )
 }
--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
+import moment from "moment"
+import { Link } from 'react-router-dom';
 
 const Jobs = () => {
     const token = useSelector((state) => state.userAuth.loginInfo.token);
@@ -65,8 +67,16 @@ const Jobs = () => {
         })();
     }, [token])
     return (
-        <div className='bscontainer'>
-            <div className='row'>
+        <div className='bscontainer-fluid'>
+            <div className='row py-5'>
+                <div className='col-12 text-right mb-5'>
+                    <Link to="create-job" className="btn bg-indigo-500 hover:bg-indigo-600 text-white" >
+                        <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                            <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                        </svg>
+                        <span className="ml-2">Create Job</span>
+                    </Link>
+                </div>
                 <div className='col-12 border'>
                     <div className="bg-white shadow-lg rounded-sm border border-slate-200 relative">
                         <header className="px-5 py-4">
@@ -86,6 +96,9 @@ const Jobs = () => {
                                                 </div>
                                             </th>
                                             <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="font-semibold text-left">JOB ID</div>
+                                            </th>
+                                            <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                                                 <div className="font-semibold text-left">JOB TITLE</div>
                                             </th>
                                             <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
@@ -96,6 +109,21 @@ const Jobs = () => {
                                             </th>
                                             <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                                                 <div className="font-semibold text-left">JobType</div>
+                                            </th>
+                                            <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="font-semibold text-left">Job Status</div>
+                                            </th>
+                                            <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="font-semibold text-left">Job Class</div>
+                                            </th>
+                                            <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="font-semibold text-left">Expiry Date</div>
+                                            </th>
+                                            <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="font-semibold text-left">Employeer</div>
+                                            </th>
+                                            <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                <div className="font-semibold text-left">Actions</div>
                                             </th>
                                         </tr>
                                     </thead>
@@ -112,6 +140,9 @@ const Jobs = () => {
                                                         </div>
                                                     </td>
                                                     <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                        <div className="text-left">{job._id}</div>
+                                                    </td>
+                                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                                                         <div className="text-left">{job.job_title}</div>
                                                     </td>
                                                     <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
@@ -123,6 +154,19 @@ const Jobs = () => {
                                                     <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                                                         <div className="text-left">{job.jobtype}</div>
                                                     </td>
+                                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                        <div className="text-left">{job.jobstatus}</div>
+                                                    </td>
+                                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                        <div className="text-left">{job.jobclass}</div>
+                                                    </td>
+                                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                        <div className="text-left">{moment(job.expiryDate).format('MM/DD/YYYY')}</div>
+                                                    </td>
+                                                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                                                        <div className="text-left">{job.employer}</div>
+                                                    </td>
+
                                                 </tr>
                                             )
                                         })}
