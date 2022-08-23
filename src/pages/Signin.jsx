73ebe738c4653ca7fc098722d8732d805cbb5491
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-08-24 00:18:54 +0500

    remove code

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -133,20 +133,7 @@ function Signin() {
                 </div>
               </form>
               {/* Footer */}
-              <div className="pt-5 mt-6 border-t border-slate-200">
-                {/* <div className="text-sm">
-                  Don’t you have an account? <Link className="font-medium text-indigo-500 hover:text-indigo-600" to="/signup">Sign Up</Link>
-                </div> */}
-                {/* Warning */}
-                {/* <div className="mt-5">
-                  <div className="bg-amber-100 text-amber-600 px-3 py-2 rounded">
-                    <svg className="inline w-3 h-3 shrink-0 fill-current mr-2" viewBox="0 0 12 12">
-                      <path d="M10.28 1.28L3.989 7.575 1.695 5.28A1 1 0 00.28 6.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 1.28z" />
-                    </svg>
-                    <span className="text-sm"> To support you during the pandemic super pro features are free until March 31st. </span>
-                  </div>
-                </div> */}
-              </div>
+
             </div>
 
           </div>
