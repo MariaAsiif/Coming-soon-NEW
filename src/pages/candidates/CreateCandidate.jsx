a067d5faea2b9b3a0d5dcdb9c0f89a06f805b26f
laherasif <laherasif@gmail.com>
2022-08-23 07:06:25 +0500

    minner change

--- src/pages/candidates/CreateCandidate.jsx
+++ src/pages/candidates/CreateCandidate.jsx
@@ -215,7 +215,7 @@ const CreateCandidate = () => {
                                         if (values.surname !== "sur")
                                             return (
                                                 <li key={i} >
-                                                    {/* <span onClick={() => handlePlaces(sur, "surname")} className=" cursor-pointer dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "  >{sur}</span> */}
+                                                    <span className=" cursor-pointer dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "  >{sur}</span>
                                                 </li>
                                             )
                                     })}
