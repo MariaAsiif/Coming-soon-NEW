e916306057aee3c67d8cddd49216c61ef577c5cf
laherasif <laherasif@gmail.com>
2022-09-06 01:39:09 -0700

    new update for dropdowns

--- src/partials/SidebarLinkGroup.jsx
+++ src/partials/SidebarLinkGroup.jsx
@@ -14,20 +14,12 @@ function SidebarLinkGroup({
     setOpen(!open);
   }
 
-  const handleSubClick = (data) => {
-    console.log("daya" , data )
-    
-    if (data === data) {
-      setSubopen(true)
 
-      // setSubopen(!subopen)
-    }
-  }
 
   //bg-gray-100
   return (
-    <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${activecondition && 'bg-[#E5E4E2]'}`}>
-      {children(handleClick, open, handleSubClick, subopen)}
+    <li className={`${level === 1 ? "px-3" : ""} py-2 rounded-sm mb-0.5 last:mb-0 ${activecondition && 'bg-[#E5E4E2]'}`}>
+      {children(handleClick, open)}
     </li>
   );
 }
