00dc791d5954b978e3111b98e7805aa711b6b0a9
laherasif <laherasif@gmail.com>
2022-09-06 01:29:36 -0700

    changes

--- src/partials/SidebarLinkGroup.jsx
+++ src/partials/SidebarLinkGroup.jsx
@@ -8,17 +8,38 @@ function SidebarLinkGroup({
 
   console.log("acc", activecondition)
   const [open, setOpen] = useState(activecondition);
+<<<<<<< Updated upstream
+=======
+  const [subopen, setSubopen] = useState();
+>>>>>>> Stashed changes
 
   const handleClick = () => {
     setOpen(!open);
   }
 
+<<<<<<< Updated upstream
 
 
   //bg-gray-100
   return (
     <li className={`${level === 1 ? "px-3" : ""} py-2 rounded-sm mb-0.5 last:mb-0 ${activecondition && 'bg-[#E5E4E2]'}`}>
       {children(handleClick, open)}
+=======
+  const handleSubClick = (data) => {
+    console.log("daya" , data )
+    
+    if (data === data) {
+      setSubopen(true)
+
+      // setSubopen(!subopen)
+    }
+  }
+
+  //bg-gray-100
+  return (
+    <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${activecondition && 'bg-[#E5E4E2]'}`}>
+      {children(handleClick, open, handleSubClick, subopen)}
+>>>>>>> Stashed changes
     </li>
   );
 }
