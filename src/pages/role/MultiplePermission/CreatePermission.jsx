143380f155fb782d1c12694910827383df70bba8
laherasif <laherasif@gmail.com>
2022-09-07 23:51:51 -0700

    roles added

--- src/pages/role/MultiplePermission/CreatePermission.jsx
+++ src/pages/role/MultiplePermission/CreatePermission.jsx
@@ -147,6 +147,12 @@ const CreateMultiplePermission = () => {
                                 </svg>
                             </li>
                             <li className="flex items-center">
+                                <Link to="/roles" className="text-slate-500 hover:text-indigo-500" >Roles </Link>
+                                <svg className="h-4 w-4 fill-current text-slate-400 mx-3" viewBox="0 0 16 16">
+                                    <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
+                                </svg>
+                            </li>
+                            <li className="flex items-center">
                                 <Link to="/multiplePermission" className="text-slate-500 hover:text-indigo-500" >Multiple Permission </Link>
                                 <svg className="h-4 w-4 fill-current text-slate-400 mx-3" viewBox="0 0 16 16">
                                     <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
