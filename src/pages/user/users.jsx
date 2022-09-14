981cb784e853fffebef95626dd6452acba820a44
Mohsin Ali <mohsinnaqvi.engr@gmail.com>
2022-09-15 04:13:34 +0500

    complete the user management

--- src/pages/user/users.jsx
+++ src/pages/user/users.jsx
@@ -263,6 +263,9 @@ const Users = () => {
                                   <path d='M19.7 8.3c-.4-.4-1-.4-1.4 0l-10 10c-.2.2-.3.4-.3.7v4c0 .6.4 1 1 1h4c.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4l-4-4zM12.6 22H10v-2.6l6-6 2.6 2.6-6 6zm7.4-7.4L17.4 12l1.6-1.6 2.6 2.6-1.6 1.6z' />
                                 </svg>
                               </button>
+                              <button className="text-slate-400 hover:text-slate-500 rounded-full" onClick={(e) => openUserPopup(e, "view", user)}>
+                                <IoEyeOutline className='text-red-500 hover:text-green-600' size={23} />
+                              </button>
                               <button
                                 className='text-rose-500 hover:text-rose-600 rounded-full'
                                 onClick={() => deletePopToggle(user._id)}
